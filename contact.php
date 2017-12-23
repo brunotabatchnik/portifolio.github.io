dc5b00fd231503872e616c7763d22dd26c9b3472
brunotabatchnik <34776075+brunotabatchnik@users.noreply.github.com>
2017-12-23 23:49:33 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST ['email'];
+$tel = $_POST['tel'];
+$message =$_POST['message'];
+
+
+$headers = "from: ". $name;
+
+$corpemail = 'Contat
+
+			name: '     .$name'
+			email: '    .$email'
+			tel: '	   .$tel'
+			message:' .$message.' ';
+
+
+if(mail("btabatchnik@gmail.com","contact",$corpemail,$headers)){
+
+	echo " <script>alert{'your message send with sucess!'};</script>";
+	header("location : index.html");
+
+}else{
+
+	echo "<script>alert{'your message not send!'};</script>";
+}
+			
+
+?>
